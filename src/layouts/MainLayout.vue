80059acdfe2f823a93c3e09596563cbd0617e62e
morancanto <2913001+niurkamc1962@users.noreply.github.com>
2025-02-23 14:34:39 -0500

    Commit que tiene el select y muestra la ip del servidor en el layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -5,16 +5,33 @@
         <q-toolbar-title class="text-h6"> Siscont - ERPNext </q-toolbar-title>
 
         <q-space />
-        <div v-if="ip_server" class="text-h6 text-white">Conectado a: {{ ip_server }}</div>
-        <q-btn @click="toggleMenu" flat icon="menu" />
+
+        <div v-if="ip_server">
+          <div class="q-pa-md">
+            Conectado a: {{ ip_server }}
+            <div class="q-gutter-md row items-start">
+              <q-select
+                filled
+                v-model="selectedOption"
+                :options="options"
+                label="Modulos"
+                style="width: 250px"
+                color="primary"
+                class="text-white"
+                @input="handleOptionChange"
+              />
+            </div>
+          </div>
+
+          <!-- <q-menu v-model="menuVisible">
+            <q-list>
+              <q-item @click="optionSelected('opcion1')">Opcion 1</q-item>
+              <q-item @click="optionSelected('opcion2')">Opcion 2</q-item>
+              <q-item @click="optionSelected('opcion3')">Opcion 3</q-item>
+            </q-list>
+          </q-menu> -->
+        </div>
       </q-toolbar>
-      <q-menu v-model="menuVisible">
-        <q-list>
-          <q-item @click="optionSelected('opcion1')">Opcion 1</q-item>
-          <q-item @click="optionSelected('opcion2')">Opcion 2</q-item>
-          <q-item @click="optionSelected('opcion3')">Opcion 3</q-item>
-        </q-list>
-      </q-menu>
     </q-header>
 
     <q-page-container>
@@ -46,32 +63,63 @@ export default defineComponent({
       menuVisible.value = !menuVisible.value
     }
 
-    const optionSelected = (option: string) => {
-      // Aqui manegar la opcion seleccionada
-      switch (option) {
-        case 'opcion1':
-          console.log('Opcion 1 ')
-          break
-        case 'opcion2':
-          console.log('Opcion 2 ')
-          break
-        case 'opcion3':
-          console.log('Opcion 3 ')
-          break
-        case 'opcion4':
-          console.log('Opcion 4')
-          break
-        default:
-          console.log('Opcion no valida')
-      }
+    console.log('menuvisible: ', menuVisible)
 
+    // estado de la opcion seleccionada
+    const selectedOption = ref<string | null>(null)
+
+    // opciones de los modulos declaradas computadas para la reactividad y rendimiento
+    const options = computed(() => [
+      {
+        label: 'Modulo 1',
+        value: 'modulo1',
+      },
+      {
+        label: 'Modulo 2',
+        value: 'modulo2',
+      },
+      {
+        label: 'Modulo 3',
+        value: 'modulo3',
+      },
+    ])
+
+    // funcion para manejar la opcion seleccionada
+    const handleOptionChange = (value: string) => {
+      console.log('Opcion seleccionada: ', value)
+      //aqui agregar logica de datos a pasar a modulosPage.vue
       menuVisible.value = false // cerrando el menu despues de seleccionar
     }
+
+    // const optionSelected = (options: string) => {
+    //   // Aqui manegar la opcion seleccionada
+    //   switch (option) {
+    //     case 'opcion1':
+    //       console.log('Opcion 1 ')
+    //       break
+    //     case 'opcion2':
+    //       console.log('Opcion 2 ')
+    //       break
+    //     case 'opcion3':
+    //       console.log('Opcion 3 ')
+    //       break
+    //     case 'opcion4':
+    //       console.log('Opcion 4')
+    //       break
+    //     default:
+    //       console.log('Opcion no valida')
+    //   }
+
+    menuVisible.value = false // cerrando el menu despues de seleccionar
+    // }
+
     return {
       currentYear,
       ip_server: computed(() => ipStore.ip_server),
       toggleMenu,
-      optionSelected,
+      selectedOption,
+      options,
+      handleOptionChange,
       menuVisible,
     }
   },
