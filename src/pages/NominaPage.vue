e1066fbd1fec1e374b230ed141cac0fddd603494
morancanto <2913001+niurkamc1962@users.noreply.github.com>
2025-03-18 22:09:38 -0400

    Modificacion de NominaPage para mostrar nombre tablas sql Nominas

--- src/pages/NominaPage.vue
+++ src/pages/NominaPage.vue
@@ -1,13 +1,76 @@
 <template>
-  <div>
-    <div v-if="ipStore.selectedOption" class="text-h6 q-mt-md">
-      NominaPage opcion seleccionada: {{ ipStore.selectedOption }}
-    </div>
+  <div class="q-pa-md">
+    <q-table title="Tablas Nomina" :rows="tablas" :columns="columns" row-key="tabla_sql">
+      <!-- Slot personalizado para las acciones -->
+      <template v-slot:body-cell-acciones="props">
+        <q-td :props="props">
+          <q-btn
+            color="primary"
+            label="Relación con tablas"
+            @click="verRelacion(props.row)"
+            class="q-mr-sm"
+          />
+          <q-btn color="secondary" label="Importar Datos" @click="importarDatos(props.row)" />
+        </q-td>
+      </template>
+    </q-table>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useIPStore } from 'src/stores/ipStore' // importando el store
+interface Tabla {
+  nombre: string
+  tabla_sql: string
+}
 
-const ipStore = useIPStore()
+const tablas: Tabla[] = [
+  { nombre: 'Trabajadores', tabla_sql: 'SCPTRABAJADORES' },
+  { nombre: 'Categorias Ocupacional', tabla_sql: 'SNOCATEGOCUP' },
+  { nombre: 'Cargos', tabla_sql: 'SNOCARGOS' },
+  { nombre: 'Tipo Trabajador', tabla_sql: 'SNOTIPOTRABAJADOR' },
+  { nombre: 'Tipo Retenciones', tabla_sql: 'SCPCONRETPAGAR' },
+  { nombre: 'Maestro Retenciones', tabla_sql: 'SCPMAESTRORETENCION' },
+  { nombre: 'Pensionados', tabla_sql: 'SNOMANTPENS' },
+  { nombre: 'Tasas de Destajo', tabla_sql: 'SNONOMENCLADORTASADESTAJO' },
+  { nombre: 'Colectivos', tabla_sql: 'SNONOMENCLADORCOLECTIVOS' },
+  { nombre: 'Submayor Vacaciones', tabla_sql: 'SNOSMVACACIONES' },
+  { nombre: 'Submayor Salarios No reclamados', tabla_sql: 'SNOSMREINTEGRONR' },
+  { nombre: 'SC-4-08', tabla_sql: 'SNOSMREINTEGRONR' },
+]
+
+const columns = [
+  {
+    name: 'nombre',
+    required: true,
+    label: 'Nombre',
+    align: 'left' as const,
+    field: (row: Tabla) => row.nombre,
+    sortable: true,
+  },
+  {
+    name: 'tabla',
+    align: 'left' as const,
+    label: 'Tabla',
+    field: (row: Tabla) => row.tabla_sql,
+    sortable: true,
+  },
+  {
+    name: 'acciones',
+    label: 'Acciones',
+    align: 'center' as const,
+    field: '',
+    sortable: false,
+  },
+]
+
+// Funciones para las acciones
+function verRelacion(tabla: Tabla) {
+  console.log('Ver relación de:', tabla.nombre)
+  // Lógica para ver la relación
+}
+
+function importarDatos(tabla: Tabla) {
+  console.log('Importar datos de:', tabla.nombre)
+  // Lógica para importar datos
+}
 </script>
