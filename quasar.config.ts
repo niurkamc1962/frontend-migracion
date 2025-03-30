6db547770d488d391861d0e28ff75446add7a307
morancanto <2913001+niurkamc1962@users.noreply.github.com>
2025-03-30 11:54:09 -0400

    Modificacion del puerto

--- quasar.config.ts
+++ quasar.config.ts
@@ -80,6 +80,7 @@ export default defineConfig((/* ctx */) => {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#devserver
     devServer: {
       // https: true,
+      port: 3000,
       open: true // opens browser window automatically
     },
 
